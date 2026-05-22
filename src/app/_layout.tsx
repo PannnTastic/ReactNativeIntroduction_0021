6f57966ed2cb4a5c367131753f70efcdbd6c4507
PannnTastic <pannntastic@gmail.com>
2026-05-22 21:03:21 +0700

    style: add StatusBar dark style to root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,17 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "index" }} />
-      <Stack.Screen name="dashboard" options={{ title: "dashboard" }} />
-      <Stack.Screen name="form" options={{ title: "form" }} />
-      <Stack.Screen name="contoh" options={{ title: "contoh" }} />
-      <Stack.Screen name="abra" options={{ title: "abra" }} />
-    </Stack>
+    <>
+      <StatusBar style="dark" />
+      <Stack>
+        <Stack.Screen name="index" options={{ title: "index" }} />
+        <Stack.Screen name="dashboard" options={{ title: "dashboard" }} />
+        <Stack.Screen name="form" options={{ title: "form" }} />
+        <Stack.Screen name="contoh" options={{ title: "contoh" }} />
+        <Stack.Screen name="abra" options={{ title: "abra" }} />
+      </Stack>
+    </>
   );
 }
